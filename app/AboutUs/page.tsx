76a993f2d8f7fed513d15971d970fe3fbf6c75ca
rorymclaughlin <rmclaughlin432@gmail.com>
2023-10-31 14:36:56 +0000

    fixed About Us and price format

--- app/AboutUs/page.tsx
+++ app/AboutUs/page.tsx
@@ -15,9 +15,7 @@ export function generateMetadata({
 
 export default function AboutUs() {
     return (
-        <Link
-          href={"/AboutUs/"}
-        >
+        <div>
           <figure>
           <Image
             src={"https://images.unsplash.com/photo-1445205170230-053b83016050?auto=format&fit=crop&q=80&w=1171&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
@@ -34,6 +32,6 @@ export default function AboutUs() {
                 <p>We prioritize the satisfaction of our customers and offer personalized assistance to ensure that your shopping journey is nothing short of exceptional. Our dedicated team of fashion experts is available to provide style advice, answer any questions you may have, and assist you throughout the purchasing process.</p>
                 <p>Glamouré is not just about luxury; it's about embracing a lifestyle that exudes sophistication and refinement. We understand that quality is paramount, which is why we curate our collection with an unwavering commitment to sourcing products that embody timeless elegance. Indulge in the world of opulence and let Glamouré be your guide to a life of lavish style. Our pun-inspired name captures the essence of our brand – combining the allure of glamour with a touch of je ne sais quoi. Get ready to immerse yourself in a world of luxury that is both inspiring and enchanting at Glamouré. Elevate your fashion game and embrace the epitome of elegance today.</p>
           </div>
-        </Link>
+          </div>
       );
 };
