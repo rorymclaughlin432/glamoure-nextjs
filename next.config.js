f993b519fd3847b5632acdcaaf06953eb23c468f
rorymclaughlin <rmclaughlin432@gmail.com>
2025-04-07 16:16:30 +0100

    Add Jest configuration file and update image domains in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains: ['i.ibb.co', 'unsplash.com', 'images.unsplash.com', 'lh3.googleusercontent.com'],
         remotePatterns: [{hostname: "images.unsplash.com"},
         {hostname: "lh3.googleusercontent.com"}]
     },
