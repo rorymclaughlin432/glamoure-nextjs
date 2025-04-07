593801bc3c9caa792298f5f4cf1570cbb3c56b78
rorymclaughlin <rmclaughlin432@gmail.com>
2025-04-07 17:48:08 +0100

    Refactor authentication options to separate authOptions into its own file and streamline imports

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,37 +1,8 @@
-import { prisma } from "@/src/lib/db/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import NextAuth from "next-auth/next";
-import { env } from "@/src/lib/env";
-import { mergeAnonymousCartIntoUserCart } from "@/src/lib/db/cart";
-import { PrismaClient } from "@prisma/client";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma as PrismaClient),
+import NextAuth from "next-auth";
+import { authOptions } from "@/src/lib/auth/authOptions";
 
-  providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    session({ session, user }) {
-      session.user.id = user.id;
-      return session;
-    },
-  },
-  events: {
-    async signIn({ user }) {
-      await mergeAnonymousCartIntoUserCart(user.id);
-    },
-  },
-};
-
-// Wrap NextAuth with a handler for Next.js API routes
 const handler = NextAuth(authOptions);
 
-// Export the handler explicitly for GET and POST methods
 export const GET = handler;
 export const POST = handler;
