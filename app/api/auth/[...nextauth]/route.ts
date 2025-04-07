934e21dd3798c3aed11c39d6e31d939b8eda36c4
rorymclaughlin <rmclaughlin432@gmail.com>
2025-04-07 17:43:25 +0100

    Refactor NextAuth options to simplify adapter usage and improve code readability

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 import { prisma } from "@/src/lib/db/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
-import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth/next";
 import { env } from "@/src/lib/env";
@@ -9,29 +8,30 @@ import { mergeAnonymousCartIntoUserCart } from "@/src/lib/db/cart";
 import { PrismaClient } from "@prisma/client";
 
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
+  adapter: PrismaAdapter(prisma as PrismaClient),
 
-    providers: [
-        GoogleProvider({
-            clientId: env.GOOGLE_CLIENT_ID,
-            clientSecret: env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    callbacks: {
-        session({session, user}) {
-            session.user.id = user.id;
-            return session;
-        }
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
     },
-
-    events: {
-        async signIn({user}) {
-            await mergeAnonymousCartIntoUserCart(user.id);
-        }
-    }
-
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeAnonymousCartIntoUserCart(user.id);
+    },
+  },
 };
 
+// Wrap NextAuth with a handler for Next.js API routes
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST}
+// Export the handler explicitly for GET and POST methods
+export const GET = handler;
+export const POST = handler;
