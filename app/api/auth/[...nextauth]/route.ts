dc7440ba36d6362f19329546b4dea076a353d75b
rorymclaughlin <rmclaughlin432@gmail.com>
2025-04-07 16:29:15 +0100

    Refactor authentication options to simplify Google provider setup and remove unused imports

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,37 +1,15 @@
-import { prisma } from "@/src/lib/db/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { NextAuthOptions } from "next-auth";
-import { Adapter } from "next-auth/adapters";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import NextAuth from "next-auth/next";
-import { env } from "@/src/lib/env";
-import { mergeAnonymousCartIntoUserCart } from "@/src/lib/db/cart";
-import { PrismaClient } from "@prisma/client";
 
-export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
-
-    providers: [
-        GoogleProvider({
-            clientId: env.GOOGLE_CLIENT_ID,
-            clientSecret: env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    callbacks: {
-        session({session, user}) {
-            session.user.id = user.id;
-            return session;
-        }
-    },
-
-    events: {
-        async signIn({user}) {
-            await mergeAnonymousCartIntoUserCart(user.id);
-        }
-    }
-
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST };
