a8c7a03c5bc31151fbb2126cf3b85d51c3c64147
rorymclaughlin <rmclaughlin432@gmail.com>
2025-04-14 12:20:59 +0100

    fixed Stripe API version causing issues with vercel

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-03-31.basil",
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2022-11-15',
 });
 
 export async function POST(req: Request) {
