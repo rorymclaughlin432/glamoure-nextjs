3e69dc958e2de0488aa3ccd47eefdeb2263a9987
rorymclaughlin <rmclaughlin432@gmail.com>
2025-04-14 12:28:23 +0100

    refactor: remove API version specification for Stripe initialization

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -5,9 +5,8 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error("Missing STRIPE_SECRET_KEY environment variable");
 }
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
 
 export async function POST(req: Request) {
   try {
