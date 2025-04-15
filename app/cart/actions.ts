bffb378e6684abb22232bc2809896b146c9709c4
rorymclaughlin <rmclaughlin432@gmail.com>
2025-04-15 09:35:09 +0100

    fixed cart actions error

--- app/cart/actions.ts
+++ app/cart/actions.ts
@@ -3,6 +3,8 @@
 import { createCart, getCart } from "@/src/lib/db/cart";
 import { prisma } from "@/src/lib/db/prisma";
 import { revalidatePath } from "next/cache";
+
+export async function setProductQuantity(productId: string, quantity: number) {
   const cart = (await getCart()) ?? (await createCart());
 
   const articleInCart = cart.items.find((item) => item.productId === productId);
